e0b4ccb67014f02ff84c81afba9bd8765f8fc3c5
r0b3rt-th0 <robert.thomas@hgs.hiddengeniusproject.org>
2025-06-25 14:52:03 -0400

    Add files via upload

---
+++ DiceView.swift
@@ -0,0 +1,36 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Robert Thomas on 6/25/25.
+//
+
+
+import SwiftUI
+
+
+struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+
+#Preview {
+    DiceView()
+}
