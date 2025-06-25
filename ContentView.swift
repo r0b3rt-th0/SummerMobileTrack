1ac8e02169a1fdcfa8e4d9827d9030ced0fa0761
r0b3rt-th0 <robert.thomas@hgs.hiddengeniusproject.org>
2025-06-25 14:52:24 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Robert Thomas on 6/25/25.
+//
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
+    var body: some View {
+        VStack {
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                    DiceView()
+                }
+            }
+            
+            HStack {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
+                    withAnimation {
+                        numberOfDice -= 1
+                    }
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
+                    withAnimation {
+                        numberOfDice += 1
+                    }
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.teal)
+        .tint(.white)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
